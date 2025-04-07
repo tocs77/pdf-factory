0f6d52ccb63c70dc1ee900be543bb3845de3fed5
Ilya Golubkov <golubkov@mcad.ru>
2025-04-07 09:00:52 +0600

    Add file upload section with styled components in MainPage and enhance header layout with home link in Layout

--- client/src/Pages/MainPage/ui/MainPage.tsx
+++ client/src/Pages/MainPage/ui/MainPage.tsx
@@ -38,8 +38,48 @@ export const MainPage = () => {
 
   return (
     <div className={classes.MainPage}>
-      <input type='file' onChange={handleFileChange} />
-      <button onClick={handleSubmit}>Upload</button>
+      <div className={classes.uploadSection}>
+        <div className={classes.fileInputWrapper}>
+          <label htmlFor='file-upload' className={classes.fileInputLabel}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'>
+              <path d='M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z'></path>
+              <polyline points='13 2 13 9 20 9'></polyline>
+            </svg>
+            Choose File
+          </label>
+          <input id='file-upload' type='file' className={classes.fileInput} onChange={handleFileChange} />
+        </div>
+
+        <div className={classes.fileNameDisplay}>{file ? file.name : 'No file selected'}</div>
+
+        <button className={classes.uploadButton} onClick={handleSubmit} disabled={!file}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'>
+            <path d='M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4'></path>
+            <polyline points='17 8 12 3 7 8'></polyline>
+            <line x1='12' y1='3' x2='12' y2='15'></line>
+          </svg>
+          Upload
+        </button>
+      </div>
+
       <div className={classes.table}>
         <Table
           columns={makeColumns(handleDeleteFile)}
