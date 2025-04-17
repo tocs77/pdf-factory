11b67ea7f4f11b50d62a544e75ad2ca891d4b456
Ilya Golubkov <golubkov@mcad.ru>
2025-04-17 12:45:58 +0600

    Update: modify ChevronLeftIcon and ChevronRightIcon

--- client/src/shared/ui/Icons/ChevronLeftIcon.tsx
+++ client/src/shared/ui/Icons/ChevronLeftIcon.tsx
@@ -7,20 +7,12 @@ interface IconProps extends React.SVGProps<SVGSVGElement> {
 
 export const ChevronLeftIcon: React.FC<IconProps> = ({ size = 16, color = 'black', ...rest }) => {
   return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width={size}
-      height={size}
-      viewBox='0 0 24 24' // Original used 32x32, using common 24x24
-      fill={color}
-      stroke='none'
-      {...rest}>
-      {/* Path from previous attempt, potentially scaled */}
+    <svg xmlns='http://www.w3.org/2000/svg' width={size} height={size} viewBox='0 0 20 20' fill='none' {...rest}>
       <path
         fillRule='evenodd'
         clipRule='evenodd'
-        d='M10 16L20 6L21.4 7.4L12.8 16L21.4 24.6L20 26L10 16Z'
-        transform='scale(0.75 0.75)' // Attempting to scale from 32x32 down to 24x24
+        d='M12.5 16.6L7.1 11.1C6.9 10.9 6.9 10.6 7.1 10.4L12.5 4.90001C12.7 4.70001 13 4.70001 13.2 4.90001C13.4 5.10001 13.4 5.40001 13.2 5.60001L8.3 10.8L13.3 15.9C13.5 16.1 13.5 16.4 13.3 16.6C13.2 16.7 13 16.8 12.9 16.8C12.8 16.8 12.6 16.7 12.5 16.6Z'
+        fill={color}
       />
     </svg>
   );
--- client/src/shared/ui/Icons/ChevronRightIcon.tsx
+++ client/src/shared/ui/Icons/ChevronRightIcon.tsx
@@ -7,20 +7,12 @@ interface IconProps extends React.SVGProps<SVGSVGElement> {
 
 export const ChevronRightIcon: React.FC<IconProps> = ({ size = 16, color = 'black', ...rest }) => {
   return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width={size}
-      height={size}
-      viewBox='0 0 24 24' // Original used 32x32, using common 24x24
-      fill={color}
-      stroke='none'
-      {...rest}>
-      {/* Path from previous attempt, potentially scaled */}
+    <svg xmlns='http://www.w3.org/2000/svg' width={size} height={size} viewBox='0 0 20 20' fill='none' {...rest}>
       <path
         fillRule='evenodd'
         clipRule='evenodd'
-        d='M21.9996 16L11.9996 26L10.5996 24.6L19.1996 16L10.5996 7.4L11.9996 6L21.9996 16Z'
-        transform='scale(0.75 0.75)' // Attempting to scale from 32x32 down to 24x24
+        d='M7.5 16.6L12.9 11.1C13.1 10.9 13.1 10.6 12.9 10.4L7.5 4.90001C7.3 4.70001 7 4.70001 6.8 4.90001C6.6 5.10001 6.6 5.40001 6.8 5.60001L11.7 10.8L6.7 15.9C6.5 16.1 6.5 16.4 6.7 16.6C6.8 16.7 7 16.8 7.1 16.8C7.2 16.8 7.4 16.7 7.5 16.6Z'
+        fill={color}
       />
     </svg>
   );
